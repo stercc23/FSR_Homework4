0af8ab995f5241302bb69df3d0c7135e280b60dc
Stefano Riccardi <stefanorcc99@gmail.com>
2024-06-09 13:49:23 +0200

    First
    And Last

---
+++ Ex3/autoRun.m
@@ -0,0 +1,44 @@
+clear all
+close all
+clc
+
+addpath fcns fcns_MPC
+%% --- parameters ---
+% ---- gait ----
+% 0-trot; 1-bound; 2-pacing 3-gallop; 4-trot run; 5-crawl
+% ---- andatura --
+% 0-trotto; 1-vincolata dx/sx; 2-vincolata fw/bw 3-galoppo; 4-trotto corsa; 5-strisciare
+
+for i = 0:1:5
+
+gait = i; 
+p = get_params(gait);
+p.playSpeed = 1;
+p.flag_movie = 1;       % 1 - make movie
+
+dt_sim = p.simTimeStep;
+SimTimeDuration = 4.5;  % [sec]
+MAX_ITER = floor(SimTimeDuration/p.simTimeStep);
+
+% desired trajectory
+p.acc_d = 1;
+p.vel_d = [0.5;0.5];
+p.yaw_d = 0;
+
+% modified parameters
+p.mass = 5.5;               % 5.5
+p.vel_d = [0.5;0];          % [0.5; 0]
+p.mu = 1.5;                   % 1
+
+prefix = "PLOT/";
+modifica = "boh/";
+cartella = modifica+"gait_"+string(gait)+"/"
+prefix = char(prefix + cartella);
+mkdir(prefix)
+
+MAIN;
+
+pause(3)
+close all
+clear all
+end
